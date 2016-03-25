1331ac1fc3bacde0696b076f70a99d92ce00f1b7
Nathan Reid <nathan-code@nathantreid.com>
2016-03-25 02:38:27 -0500

    expand the example with another local and global template and reorganize files

--- client/hello/hello.js
+++ client/hello/hello.js
@@ -1,8 +1,10 @@
 import hello from './hello.html';
 import styles from './hello.m.css';
+import message from './message.html';
 
 hello.viewmodel({
   styles,
+  message,
   counter: 0,
   incrementCounter() {
     this.counter(this.counter() + 1);
--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
-import hello from './hello.html';
+import hello from './hello/hello.html';
 import info from './info.html';
 
 Template.body.helpers({
