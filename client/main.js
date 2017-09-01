9b117b6a6006eadf301cf113627141e932de6b00
Nathan Reid <nathan-code@nathantreid.com>
2017-09-01 18:22:05 -0500

    discuss using components from the imports folder as well using components without a name

--- client/main.js
+++ client/main.js
@@ -1,8 +1,13 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
-import hello from './hello/hello.html';
+
+// Import html-only components directly:
 import info from './info.html';
+
+// Import components with a backing javascript file via the JS file:
+// Make sure to export the component from the javascript file!
+import hello from '/imports/client/hello/hello';
 
 Template.body.helpers({
   hello,
--- imports/client/hello/hello.js
+++ imports/client/hello/hello.js
@@ -1,6 +1,10 @@
 import hello from './hello.html';
 import styles from './hello.m.css';
 import message, {message_two} from './message.html';
+import './hello_message.html';
+
+// Make sure to export the component so that it can be accessed by importing the js file (see import in main.js)
+export default hello;
 
 hello.helpers({
   message,
